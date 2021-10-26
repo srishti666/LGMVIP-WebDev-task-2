bd17fba4c5af70108032edef175ad9b1ea18fa43
Srishti <srishtimanna6@gmail.com>
2021-10-27 00:32:14 +0530

    task 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const data = await response.json();
+    setUsers(data.data);
+  }
 
-export default App;
+  return (
+    <>
+      <header className="Navbar">
+      </header>
+      <div className="heading">
+        <h1>LGM Community</h1>
+      </div>
+
+      <div className="container">
+        <button className="btn" onClick={getUsers}>Get Users</button>
+        {users?.map((present, index)=> {
+          return (
+            <div className="card" key={index}>
+              <img src={present.avatar} alt="" />
+              <div className="card-back">
+                <h3 className="card-title">
+                  {present.first_name} {present.last_name}
+                </h3>
+                <h4 className="card-item">{present.email}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  )
+}
+export default App
